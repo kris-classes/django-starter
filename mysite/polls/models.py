8ea366b3201851840d83f5250f97462c7c32ac19
Nick Do <linh.do@tyme.com>
2022-12-09 07:12:23 +0700

    initial commit

---
+++ mysite/polls/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return f"Person {self.name}"
